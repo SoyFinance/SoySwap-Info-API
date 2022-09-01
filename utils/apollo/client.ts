e49ebe35e8210c78dd7c2cd289e861adb2a07af7
MJ <80132692+MJ-UKR@users.noreply.github.com>
2022-09-01 20:07:11 +0300

    update subgraph host and endpoints

--- utils/apollo/client.ts
+++ utils/apollo/client.ts
@@ -4,7 +4,7 @@ import fetch from "cross-fetch";
 export const client = new ApolloClient({
   link: new HttpLink({
     fetch,
-    uri: "http://65.21.54.13:8000/subgraphs/name/soyfinance",
+    uri: "https://03.callisto.network/subgraphs/name/soyswap",
   }),
   cache: new InMemoryCache(),
 });
--- utils/blocks/client.ts
+++ utils/blocks/client.ts
@@ -4,7 +4,7 @@ import fetch from "cross-fetch";
 export const blockClient = new ApolloClient({
   link: new HttpLink({
     fetch,
-    uri: "http://65.21.54.13:8000/subgraphs/name/blocks",
+    uri: "https://03.callisto.network/subgraphs/name/blocks",
   }),
   cache: new InMemoryCache(),
 });
